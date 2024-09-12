67125924f2ac4c0a61fdfcc39b9483ae971984a3
FullstackSena <fullstackcolegiosena@gmail.com>
2024-09-12 16:14:07 -0500

    Update userController.js

--- Colegio/src/controllers/userController.js
+++ Colegio/src/controllers/userController.js
@@ -2,8 +2,8 @@ const User = require('../models/user');
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const usersFound = await User.find();
+    res.json(usersFound);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
